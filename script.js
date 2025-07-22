910a14adaa4e2572ffde455b193d6938ab555e31
Aamirr1602 <abdulaamirr16@gmail.com>
2025-07-22 02:42:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 const messages = [
-  "Day 1: I’d sit in the rain if it meant you'd smile once more 🌧️",
-  "Day 2: Your name sounds better in my head than any song ever played 🎶",
-  "Day 3: You’re the calm in my chaos — like moonlight on a wild sea 🌙",
-  "Day 4: If I could, I’d wrap this whole sky around you like a hug ☁️",
-  "Day 5: You deserve kisses under storms and love without questions 💗",
-  "Day 6: Even when you’re quiet, I can hear your heart — and it’s beautiful 🫀",
-  "Day 7: I hope this made you feel a little lighter, a little loved 💌"
+  "Day 1 I’d sit in the rain if it meant you'd smile once more 🌧️",
+  "Day 2 Your name sounds better in my head than any song ever played 🎶",
+  "Day 3 You’re the calm in my chaos like moonlight on a wild sea 🌙",
+  "Day 4 If I could, I’d wrap this whole sky around you like a hug ☁️",
+  "Day 5 You deserve kisses under storms and love without questions 💗",
+  "Day 6 Even when you’re shh, I can hear your heart calling me and it’s beautiful 🫀",
+  "Day 7 I hope this made you feel a little lighter, a little loved 💌"
 ];
 
 function showMessage(index) {
